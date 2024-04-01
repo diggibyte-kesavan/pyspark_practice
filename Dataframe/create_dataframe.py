4ca427507bc6d25516fbd77d79d8c6a497a05a2c
diggibyte-kesavan <kesavan.k@diggibyte.com>
2024-04-01 15:01:25 +0530

    changed the name data in dataframe in create_dataframe.py file

--- Dataframe/create_dataframe.py
+++ Dataframe/create_dataframe.py
@@ -6,7 +6,7 @@ data = [("James", "Smith", "31-07-2001", "M", 60000),
         ("Michael", "Rose", "2-08-2003", "M", 70000),
         ("Robert", "Williams", "09-07-2001", "M", 400000),
         ("Maria", "Anne", "24-08-2002", "F", 500000),
-        ("Jen", "Mary", "2-08-2012", "F", 2000)]
+        ("Jency", "Mary", "2-08-2012", "F", 2000)]
 
 columns = ["first_name", "last_name", "dob", "gender", "salary"]
 
