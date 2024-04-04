033a84ac2f1baba95183211b3c2d3a58da4e983c
diggibyte-kesavan <kesavan.k@diggibyte.com>
2024-04-04 12:55:36 +0530

    added the Rdd/union_unionall_operation.py and cread two rdd and did the union operation

--- Rdd/union_unionall_operation.py
+++ Rdd/union_unionall_operation.py
@@ -6,4 +6,5 @@ rdd = spark.sparkContext.parallelize([1, 2, 3, 4])
 rdd1= spark.sparkContext.parallelize([3, 4, 5, 6])
 
 union = rdd.union(rdd1)
+# union1 = rdd.union(rdd1).distinct() # using distinct keyword we remove the duplicate values
 print(union.collect())
