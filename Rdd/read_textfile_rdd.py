a93230f5933fc796107c66a54ab004f8535b7f66
diggibyte-kesavan <kesavan.k@diggibyte.com>
2024-04-04 12:52:47 +0530

    added the  Rdd/read_textfile_rdd.py

--- Rdd/read_textfile_rdd.py
+++ Rdd/read_textfile_rdd.py
@@ -12,12 +12,12 @@ print(rdd.collect())
 rdd1 = spark.sparkContext.textFile(r'C:\Users\kesav\Documents\DE_Intern_Diggibyte\dataset\username.csv')
 print(rdd1.collect())
 
-# read csv file2 with give the 1 column value
+# read csv(,) file2 with give the 1 column value
 
 rdd2 = spark.sparkContext.textFile(r'C:\Users\kesav\Documents\DE_Intern_Diggibyte\dataset\sample_csv_data2.csv')
 print(rdd2.collect())
 
 # read the csv file using multiple columns like structured 
 
-rdd3 =spark.sparkContext.textFile(r'C:\Users\kesav\Downloads\addresses.csv')
+rdd3 =spark.sparkContext.textFile(r'C:\Users\kesav\Documents\DE_Intern_Diggibyte\dataset\addresses.csv')
 print(rdd3.collect())
