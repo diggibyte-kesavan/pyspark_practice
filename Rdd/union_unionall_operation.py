f5abeebafc96b98ec3798de5f387ba2d5d77e2d9
diggibyte-kesavan <kesavan.k@diggibyte.com>
2024-04-08 14:47:58 +0530

    practice the action fucntions in union and union all file

--- Rdd/union_unionall_operation.py
+++ Rdd/union_unionall_operation.py
@@ -4,6 +4,10 @@ spark = SparkSession.builder.appName('union').getOrCreate()
 
 rdd = spark.sparkContext.parallelize([1, 2, 3, 4])
 rdd1= spark.sparkContext.parallelize([3, 4, 5, 6])
+print(rdd.first())
+print(rdd.count())
+print(rdd.take(2))
+print(rdd.max())
 
 union = rdd.union(rdd1)
 # union1 = rdd.union(rdd1).distinct() # using distinct keyword we remove the duplicate values
